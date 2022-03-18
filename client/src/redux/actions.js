702faedd0874f92701862ee80dfa4609e05aa236
daviddflix <david-972010@hotmail.com>
2022-03-18 04:39:30 -0300

    changing route

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -15,7 +15,7 @@ export function getCountries (title){
     if(title){
        return async function (dispatch){
           try {
-               const res = await axios.get(`http://localhost:3001/countries?name=${title}`); 
+               const res = await axios.get(`https://countriesa-d.herokuapp.com/countries?name=${title}`); 
                console.log(res)
                    return dispatch({ type: GET_COUNTRY, payload: res.data });
            } catch (err) {
@@ -29,7 +29,7 @@ export function getCountries (title){
 
  export function getDetail(idPais){
     return async function (dispatch){
-       const data = await fetch(`http://localhost:3001/countries/${idPais}`)
+       const data = await fetch(`https://countriesa-d.herokuapp.com/countries/${idPais}`)
        const info = await data.json()
        return dispatch({ type: GET_DETAILS, payload: info })
     }        
@@ -38,7 +38,7 @@ export function getCountries (title){
  export function getAll (){
   return async function (dispatch){
          try {
-              const res = await axios.get(`http://localhost:3001/countries`);
+              const res = await axios.get(`https://countriesa-d.herokuapp.com/countries`);
               return dispatch({ type: GET_All, payload: res.data });
           } catch (err) {
               return console.error(err);
@@ -54,7 +54,7 @@ export function getCountries (title){
  export function postAcitvity  (payload){
  return function  (dispatch){
     try {
-        const res =   axios.post("http://localhost:3001/activity", payload);
+        const res =   axios.post("https://countriesa-d.herokuapp.com/activity", payload);
          return res
     } catch (err) {
         return console.error(err);
@@ -66,7 +66,7 @@ export function getCountries (title){
  export function  getActs(){
     return async function(dispatch){
        try {
-           const res = await axios.get("http://localhost:3001/activities")
+           const res = await axios.get("https://countriesa-d.herokuapp.com/activities")
            return dispatch({ type: GET_ACTS, payload: res.data })
        } catch (error) {
            console.log(error)
